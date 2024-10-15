970dba3e6c6e92248adc17bc26373cc35a791f26
Adahan00-kg <20ikramidinovrinat07@gmail.com>
2024-10-15 14:00:54 +0600

    project

---
+++ mysite/online_store/models.py
@@ -0,0 +1,109 @@
+
+from django.contrib.auth.models import AbstractUser
+
+from django.db import models
+
+from django.core.validators import MaxLengthValidator,MinValueValidator
+from phonenumber_field.modelfields import PhoneNumberField
+
+class UserProfile(AbstractUser):
+
+    age = models.PositiveSmallIntegerField(null=True,blank=True,
+                                           validators=[MinValueValidator(15),
+                                                       MaxLengthValidator(110)])
+    country = models.CharField(max_length=32,null=True,blank=True)
+    phono_number = PhoneNumberField(null=True,blank=True,region='KG')
+    date_register = models.DateField(auto_now_add=True,null=True,blank=True)
+    STATUS_CHOiCES = (
+        ('gold','Gold'),
+        ('silver','Silver'),
+        ('bronze','Bronze'),
+        ('simple','Simple')
+    )
+
+    status = models.CharField(max_length=10,choices=STATUS_CHOiCES,default='simple')
+
+    def __str__(self):
+        return f'{self.first_name} - {self.last_name}'
+
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=16 )
+
+    def __str__(self):
+        return self.category_name
+
+class Product(models.Model):
+    product_name = models.CharField(max_length=32,unique=True)
+    category = models.ForeignKey(Category,related_name='products',on_delete=models.CASCADE)
+    price = models.PositiveIntegerField(default=0)
+    description = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    active  = models.BooleanField(default=True,verbose_name='в наличии')
+    product_video = models.FileField(upload_to='vid/',verbose_name='видео',null=True,blank=True)
+    owner = models.ForeignKey(UserProfile,on_delete=models.CASCADE,null=True,blank=True)
+
+    def __str__(self):
+        return self.product_name
+
+    def get_average_rating(self):
+        ratings = self.rating.all()
+        if ratings.exists():
+            return (round(sum(rating.stars for rating in ratings) / ratings.count(), 1))
+        return 0
+
+
+class ProductPhoto(models.Model):
+    product = models.ForeignKey(Product,related_name='product',on_delete=models.CASCADE)
+
+
+class Rating(models.Model):
+    product = models.ForeignKey(Product,on_delete=models.CASCADE,related_name='ratings')
+    user  = models.ForeignKey(UserProfile,on_delete=models.CASCADE)
+    stars = models.PositiveSmallIntegerField(choices=[(i,str(i)) for i in range(6)],verbose_name='Рейтинг')
+
+
+    def __str__(self):
+        return f'{self.product} - {self.user} - {self.stars}'
+
+
+class Review(models.Model):
+    author = models.ForeignKey(UserProfile,on_delete=models.CASCADE)
+    product = models.ForeignKey(Product,related_name='reviews',on_delete=models.CASCADE)
+    text = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    parent_review = models.ForeignKey('self', related_name='replies', null=True, blank=True, on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return f'{self.author} - {self.text}'
+
+
+class Cart(models.Model):
+    user = models.OneToOneField(UserProfile, on_delete=models.CASCADE)
+    created_date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.user}'
+
+    def get_total_price(self):
+        total_price = sum(item.get_total_price() for item in self.items.all())
+        discount = 0
+        if self.user.status == 'gold':
+            discount = 0.75
+        elif self.user.status == 'silver':
+            discount = 0.5
+        elif self.user.status == 'bronze':
+            discount = 0.25
+        final_price = total_price *(1-discount)
+        return final_price
+
+
+class CarItem(models.Model):
+    cart = models.ForeignKey(Cart, related_name='items', on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveSmallIntegerField(default=1)
+
+    def get_total_price(self):
+        return self.product.price * self.quantity
+
---
+++ mysite/online_store/serializers.py
@@ -0,0 +1,134 @@
+from calendar import month
+
+from django.core.serializers import serialize
+from django.db.models import DateField
+from django.contrib.auth.models import User
+from rest_framework import serializers
+from .models import *
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.contrib.auth import authenticate
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserProfile
+        fields = ['username', 'email', 'password','first_name', 'last_name', 'age',
+        'date_register', 'phono_number', 'status']
+
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = UserProfile.objects.create_user(**validated_data)
+        return user
+
+    def to_representation(self, instance):
+        refresh = RefreshToken.for_user(instance)
+        return {
+            'user': {
+                'username': instance.username,
+                'email': instance.email,
+            },
+            'access': str(refresh.access_token),
+            'refresh': str(refresh),
+        }
+
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True)
+
+    def validate(self, data):
+        user = authenticate(**data)
+        if user and user.is_active:
+            return user
+        raise serializers.ValidationError("Неверные учетные данные")
+
+    def to_representation(self, instance):
+        refresh = RefreshToken.for_user(instance)
+        return {
+            'user': {
+                'username': instance.username,
+                'email': instance.email,
+            },
+            'access': str(refresh.access_token),
+            'refresh': str(refresh),
+        }
+
+
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserProfile
+        fields = ['first_name','last_name']
+
+
+class ProductPhotoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProductPhoto
+        fields = '__all__'
+
+
+
+class RatingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Rating
+        fields = '__all__'
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    created_date = serializers.DateTimeField(format='%d - %m - %Y  %H:%M')
+    class Meta:
+        model = Review
+        fields = '__all__'
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['category_name']
+
+class ProductListSerializer(serializers.ModelSerializer):
+    date = serializers.DateTimeField(format='%d-%m-%Y')
+    category = CategorySerializer()
+    class Meta:
+        model = Product
+        fields = ['id','product_name','price','date','category']
+
+
+
+class ProductDetailSerializer(serializers.ModelSerializer):
+
+    owner = UserProfileSerializer()
+    category = CategorySerializer()
+    product = ProductPhotoSerializer(many=True,read_only=True)
+    ratings = RatingSerializer(many=True,read_only=True)
+    reviews = ReviewSerializer(many=True,read_only=True)
+    date = serializers.DateTimeField(format='%d-%m-%Y')
+    class Meta:
+        model = Product
+        fields = ['product_name','description','category','price','active','product_video',
+                  'date','owner','product','ratings','reviews']
+
+    # def get_average_rating(self,obj):
+    #     return obj.get_average_rating()
+
+
+
+class CartItemSerializer(serializers.ModelSerializer):
+    product = ProductListSerializer(read_only=True)
+    product_id = serializers.PrimaryKeyRelatedField(queryset=Product.objects.all(),write_only=True,source='product')
+
+    class Meta:
+        model = CarItem
+        fields = ['id','product','product_id','get_total_price']
+
+class CartSerializer(serializers.ModelSerializer):
+    items = CartItemSerializer(many=True,read_only=True)
+    total_price = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Cart
+        fields = ['id','user','items','total_price']
+
+
+    def get_total_price(self,obj):
+        return obj.get_total_price
